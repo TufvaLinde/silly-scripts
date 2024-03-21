aa0cbe43dbe2e2377837d563d5683a4780a3758a
Tufva <tufva@tufvalinde.com>
2024-03-21 23:41:46 +0100

    small changes

--- room-of-requirement/ror.py
+++ room-of-requirement/ror.py
@@ -1,7 +1,21 @@
+'''
+args:
+    year month day time duration
+    alternatively: 
+        use rn for now and add duration 
+        use td for today and add time and duration
+    example:
+    python3 ror.py 2024 03 20 8 2   // find me a classroom at 8 am on march 20th 2024 that I can use for 2 hours
+    python3 ror.py rn 4              // -||- right now that I can use for 4 hours
+    python3 ror.py td 10 6           // -||- today that I can use for 6 hours
+
+'''
+
 from ics import Calendar
 import sys
 import requests
 from datetime import datetime, timedelta
+from itertools import zip_longest
 
 def parse_events(calendar, desired_time, duration):
     occupied_classrooms = set()
@@ -16,23 +30,22 @@ def parse_events(calendar, desired_time, duration):
 url = "https://cloud.timeedit.net/kth/web/stud02/ri.ics?sid=7&p=4&objects=353305.4%2C353278%2C353279%2C411281%2C353280%2C353274%2C353275%2C353276%2C373913%2C446809%2C353281%2C353283%2C353264%2C353272%2C353304%2C353269%2C353267%2C353266%2C353268%2C-1%2C353270.4%2C353271%2C353303%2C353299%2C353298%2C353347%2C353344%2C353343%2C353342%2C353382%2C353380%2C353384%2C353383%2C353381%2C353385%2C353386%2C353387%2C353388%2C353389%2C353397%2C-1%2C353396.4%2C353409%2C353399%2C353400%2C353402%2C353401%2C353398%2C353405%2C353407%2C353406%2C353408%2C353412%2C353414%2C353411%2C353413%2C399482%2C399465%2C399466%2C399472%2C399474%2C-1%2C399481.4%2C353326%2C353327%2C353328%2C353331%2C353333%2C353330%2C353340%2C353341%2C353339%2C353338%2C387925%2C387922%2C387929%2C387927&l=sv&e=2403&ku=19900&k=578E00997A5AF366F7BA048002F061A78E"
 calendar = Calendar(requests.get(url).text)
 
-if sys.argv[1] == "n": 
+if sys.argv[1] == "rn": 
     desired_time = datetime.now()
-    duration = timedelta(hours=float(sys.argv[2]))
-elif sys.argv[1] == "t":
+elif sys.argv[1] == "td":
     year = datetime.now().year
     month = datetime.now().month
     day = datetime.now().day
     hour = int(sys.argv[2])
-    duration = timedelta(hours=float(sys.argv[3]))
     desired_time = datetime(year, month, day, hour, 0)
 else:
     year = int(sys.argv[1])
     month = int(sys.argv[2])
     day = int(sys.argv[3])
     hour = int(sys.argv[4])
-    duration = timedelta(hours=float(sys.argv[5]))
     desired_time = datetime(year, month, day, hour, 0)
+
+duration = timedelta(hours=float(sys.argv[-1]))
 
 occupied_classrooms = parse_events(calendar, desired_time, duration)
 
@@ -48,5 +61,10 @@ all_classrooms = [
 
 free_classrooms = [room for room in all_classrooms if room not in occupied_classrooms]
 
+grouped_classrooms = {}
 for room in free_classrooms:
-    print(room)
+    letter = room[0]
+    grouped_classrooms.setdefault(letter, []).append(room)
+
+for rooms in zip_longest(*grouped_classrooms.values(), fillvalue=""):
+    print("\t".join(rooms))
